0ec435c1540beebbcd22c48764a7b2acbb9eeb67
Endryus-Montezano <endryusmontezano@live.com>
2025-07-01 16:27:35 -0300

    Função de soma e igual

--- desafio01-calculadora/src/App.js
+++ desafio01-calculadora/src/App.js
@@ -7,13 +7,41 @@ import { useState } from 'react';
 
 const App = () => {
   const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
 
   const handleAddNumber = (num) => {
-    setCurrentNumber(prev => `${num}${prev === '0' ? '' : prev}`)
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
   }
 
   const handleOnClear = () => {
-    setCurrentNumber('0')
+    setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation('');
+  }
+
+  const handleSumNumbers = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('+')
+    }else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('');
+    }
+  }
+
+  const handleEquals = () => {
+    if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
+      switch(operation) {
+        case '+':
+          handleSumNumbers();
+          break;
+        default:
+          break;
+      }
+    }
   }
 
   return (
@@ -36,13 +64,13 @@ const App = () => {
             <Button label="4" onClick={() => handleAddNumber('4')}/>
             <Button label="5" onClick={() => handleAddNumber('5')}/>
             <Button label="6" onClick={() => handleAddNumber('6')}/>
-            <Button label="+" onClick={() => handleAddNumber('+')}/>
+            <Button label="+" onClick={handleSumNumbers}/>
         </Row>
         <Row>
             <Button label="1" onClick={() => handleAddNumber('1')}/>
             <Button label="2" onClick={() => handleAddNumber('2')}/>
             <Button label="3" onClick={() => handleAddNumber('3')}/>
-            <Button label="=" onClick={() => handleAddNumber('=')}/>
+            <Button label="=" onClick={handleEquals}/>
         </Row>
         </Content>
     </Container>
